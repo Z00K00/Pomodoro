14e1799132ca2c79be556ca7aa762fd30c595ae6
Kraig Larson <147437185+Z00K00@users.noreply.github.com>
2024-03-01 12:49:46 -0500

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Krajg Larson on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
